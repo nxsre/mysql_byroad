9ded5b43d5de1e607b82c52da9937dde18ab2882
pastor <pastor.xin@gmail.com>
2016-11-03 17:33:42 +0800

    fix monitor binlog check

--- monitor/dispatcher.go
+++ monitor/dispatcher.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"mysql_byroad/model"
 	"sort"
+	"strings"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -190,12 +191,27 @@ func (dm *DispatcherManager) RunBinlogCheck() {
 func checkBinlog(dispatcher *RPCClient) {
 	masterStatus, err := dispatcher.GetMasterStatus()
 	if err != nil {
+		log.Errorf("get %s master status error: %s", dispatcher.Desc, err.Error())
+		return
 	}
 	currentStatus, err := dispatcher.GetCurrentBinlogInfo()
 	if err != nil {
+		log.Errorf("get %s current status error: %s", dispatcher.Desc, err.Error())
+		return
 	}
-	if masterStatus.Position-currentStatus.Position > Conf.AlertConfig.BinlogPosGap {
+	if isAlert(masterStatus, currentStatus) {
 		content := fmt.Sprintf("旁路系统\n时间：%s\n数据库实例：%s\nmaster status: %+v\ncurrent status: %+v", time.Now().String(), dispatcher.Desc, masterStatus, currentStatus)
 		SendAlert(content)
 	}
+}
+
+/*
+根据binlog的filename和position判断是否需要报警
+*/
+func isAlert(masterStatus, currentStatus *model.BinlogInfo) bool {
+	if masterStatus.Position-currentStatus.Position > Conf.AlertConfig.BinlogPosGap ||
+		(strings.Compare(masterStatus.Filename, currentStatus.Filename) > 0 && currentStatus.Position-masterStatus.Position < Conf.AlertConfig.BinlogPosGap) {
+		return true
+	}
+	return false
 }
